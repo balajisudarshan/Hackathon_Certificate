988147c2dc17e35c5331c2461f24834090f2f083
Eswar <gadiparthieswar@gmail.com>
2026-01-22 18:23:57 +0530

    ratelimiter

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth.route");
+const rateLimiter = require("./middleware/RateLimiter.middleware");
 const certificateRoutes = require("./routes/certificate.route");
 const connectDb = require("./config/db");
 const app = express();
 require("dotenv").config();
 app.use(cors());
+app.use(rateLimiter);
 
 app.use(express.json());
 app.use("/api/certificates", certificateRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT;
 connectDb().then(() => {
